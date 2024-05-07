72611032e823886e353830ca5f1b1d9066321141
Resta134 <149498121+Resta134@users.noreply.github.com>
2024-05-07 16:27:18 +0700

    Update soal_4perhitung.py

--- soal_4perhitung.py
+++ soal_4perhitung.py
@@ -5,9 +5,9 @@ def hitung_bmi(berat, tinggi):
 def rekomendasi_kesehatan(bmi):
     if bmi < 18.5:
         return "Berat badan kurang"
-    elif bmi >= 18.5 and bmi < 25:
+    elif bmi >= 18.5 and bmi < 24.9:
         return "Berat badan normal"
-    elif bmi >= 25 and bmi < 30:
+    elif bmi >= 25 and bmi < 29.9:
         return "Kelebihan berat badan"
     else:
         return "Obesitas"
